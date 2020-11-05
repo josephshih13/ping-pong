aa4794f6355cac11fb4b6bb07fbd1dd2cd6256f5
EC2 Default User <ec2-user@ip-172-31-92-164.ec2.internal>
2020-11-05 07:13:52 +0000

    combine yaml

--- ping-pong.go
+++ ping-pong.go
@@ -4,18 +4,27 @@ import (
 	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"io/ioutil"
 	"net/http"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 var cnt = 0
 
 func getCnt(c echo.Context) error {
-	
+
 	ret := fmt.Sprintf("Pong %d\n", cnt)
+	d1 := []byte(ret)
+	err := ioutil.WriteFile("/home/ec2-user/environment/pong.txt", d1, 0644)
+	check(err)
 	cnt = cnt + 1
 	return c.String(http.StatusOK, ret)
 }
-
 
 func main() {
 	// Echo instance
@@ -23,7 +32,7 @@ func main() {
 	// var cnt *int
 	// cnt = new(int)
 	// *cnt = 12345
-	
+
 	e := echo.New()
 
 	// Middleware
