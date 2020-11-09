51a3f09257055b0aed7ad4f885e62a998c23f332
EC2 Default User <ec2-user@ip-172-31-92-164.ec2.internal>
2020-11-09 07:09:23 +0000

    become a service

--- ping-pong.go
+++ ping-pong.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"io/ioutil"
+	// "io/ioutil"
 	"net/http"
 )
 
@@ -19,10 +19,19 @@ var cnt = 0
 func getCnt(c echo.Context) error {
 
 	ret := fmt.Sprintf("Pong %d\n", cnt)
-	d1 := []byte(ret)
-	err := ioutil.WriteFile("/home/ec2-user/environment/pong.txt", d1, 0644)
-	check(err)
+	// d1 := []byte(ret)
+	// err := ioutil.WriteFile("/home/ec2-user/environment/pong.txt", d1, 0644)
+	// check(err)
 	cnt = cnt + 1
+	return c.String(http.StatusOK, ret)
+}
+
+func getCnt2(c echo.Context) error {
+
+	ret := fmt.Sprintf("Pong %d\n", cnt)
+	// d1 := []byte(ret)
+	// err := ioutil.WriteFile("/home/ec2-user/environment/pong.txt", d1, 0644)
+	// check(err)
 	return c.String(http.StatusOK, ret)
 }
 
@@ -40,7 +49,8 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Route => handler
-	e.GET("/", getCnt)
+	e.GET("/pong", getCnt)
+	e.GET("/internal", getCnt2)
 
 	// Start server
 	fmt.Println("Start Server from port 9936")
