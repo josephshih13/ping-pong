b1ecffabdf181da425a12d6aec1911492a5bb92d
EC2 Default User <ec2-user@ip-172-31-92-164.ec2.internal>
2020-11-16 07:22:43 +0000

    add db

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/jackc/pgx/v4 v4.9.2 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
--- ping-pong.go
+++ ping-pong.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	// "io/ioutil"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v4"
 )
+
+var conn *pgx.Conn
 
 func check(e error) {
 	if e != nil {
@@ -14,33 +19,61 @@ func check(e error) {
 	}
 }
 
-var cnt = 0
+func createtable() {
+	_, err := conn.Exec(context.Background(), "CREATE TABLE ping (ping_count INT NOT NULL);INSERT INTO ping (ping_count)VALUES(0);")
+	check(err)
+}
+
+func check_table_exist() bool {
+	var exist bool
+	err := conn.QueryRow(context.Background(), "SELECT EXISTS (SELECT FROM information_schema.tables WHERE  table_name = 'ping');").Scan(&exist)
+	check(err)
+	return exist
+}
+
+func read_number() int {
+	if !check_table_exist() {
+		createtable()
+		return 0
+	}
+	var num int
+	err := conn.QueryRow(context.Background(), "SELECT * from ping;").Scan(&num)
+	check(err)
+	return num
+}
+
+func add_number() {
+	num := read_number()
+	num = num + 1
+	_, err := conn.Exec(context.Background(), "UPDATE ping SET ping_count = $1;", num)
+	check(err)
+}
 
 func getCnt(c echo.Context) error {
-
+	cnt := read_number()
 	ret := fmt.Sprintf("Pong %d\n", cnt)
-	// d1 := []byte(ret)
-	// err := ioutil.WriteFile("/home/ec2-user/environment/pong.txt", d1, 0644)
-	// check(err)
-	cnt = cnt + 1
+	add_number()
 	return c.String(http.StatusOK, ret)
 }
 
 func getCnt2(c echo.Context) error {
-
+	cnt := read_number()
 	ret := fmt.Sprintf("Pong %d\n", cnt)
-	// d1 := []byte(ret)
-	// err := ioutil.WriteFile("/home/ec2-user/environment/pong.txt", d1, 0644)
-	// check(err)
 	return c.String(http.StatusOK, ret)
 }
 
 func main() {
-	// Echo instance
 
-	// var cnt *int
-	// cnt = new(int)
-	// *cnt = 12345
+	var err error
+	db_url := os.Getenv("DB_URL")
+	if db_url == "" {
+		db_url = "postgresql://postgres:test1234@joseph-public.chld9kh33qyg.us-east-1.rds.amazonaws.com:5432/postgres"
+	}
+	conn, err = pgx.Connect(context.Background(), db_url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connection to database: %v\n", err)
+		os.Exit(1)
+	}
 
 	e := echo.New()
 
